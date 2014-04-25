cc6e562c6494f70c77e55ab4b12996f7331d4a9a
Whales <fivedozenwhales@gmail.com>
2014-04-25 12:16:26 -0500

    More work on building interface.  Keybindings added.

--- city.cpp
+++ city.cpp
@@ -26,10 +26,35 @@ void City::interface_buildings
   }
 
   std::vector<std::string> building_names, building_count;
+  std::vector<Building_type> building_types; // To key name list to types
 
   for (int i = 0; i < BUILD_MAX; i++) {
     if (buildings[i] > 0) {
       building_names.push_back( Building_data[i]->name );
+      building_count.push_back( itos(buildings[i])     );
+      building_types.push_back( Building_type(i)       );
+    }
+  }
+
+  i_buildings.set_data("list_buildings", building_names);
+  i_buildings.set_data("list_count",     building_count);
+  i_buildings.select("list_buildings");
+
+  bool done = false;
+  while (!done) {
+    i_buildings.draw(&w_buildings);
+    w_buildings.refresh();
+
+    int index = i_buildings.get_int("list_buildings");
+    Building_type type = building_types[index];
+
+    i_buildings.set_data("num_maintenance", Building_data[type]->upkeep);
+
+    long ch = input();
+    if (ch == 'q' || ch == 'Q') {
+      done = true;
+    } else {
+      i_buildings.handle_keypress(ch);
     }
   }
 }
