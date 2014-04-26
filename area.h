6ef6028a957a9948e7f95e4c4710e781be52c79b
Whales <fivedozenwhales@gmail.com>
2014-04-26 17:16:56 +0000

    Add area.h - on-map areas for certain crucial buildings.

---
+++ area.h
@@ -0,0 +1,42 @@
+#ifndef _AREA_H_
+#define _AREA_H_
+
+/* Areas are similar to buildings, except they're placed on the city map.  As
+ * such, since their placement is very limited (and dependent on city growth),
+ * areas should a) be very important, and b) support a lot of citizens.
+ * Examples include living areas, farms, mines, etc.
+ */
+
+#include "glyph.h"
+#include "building.h"
+#include <string>
+
+enum Area_type
+{
+  AREA_NULL = 0,
+
+// Housing
+  AREA_HOVELS,
+  AREA_HOUSES,
+  AREA_MANOR,
+  AREA_PALACE,  // Always the center tile; always start with this
+
+// Raw resource production
+  AREA_FARM,
+  AREA_QUARRY,
+  AREA_MINE,
+
+// Military
+  AREA_BARRACKS,
+
+  AREA_MAX
+};
+
+class Area
+{
+  std::string name;
+  glyph symbol;
+  Building_type building;
+};
+
+#endif
