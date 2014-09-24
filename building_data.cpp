0dafeff305863bbdc5a6caf0a22b17f47d83de09
Whales <fivedozenwhales@gmail.com>
2014-09-24 00:56:59 -0400

    Data for how long buildings take to build.

--- building_data.cpp
+++ building_data.cpp
@@ -9,6 +9,8 @@ Building_datum* Building_data[BUILD_MAX];
   Building_data[cur_id]->name = (n)
 #define _cost(t, n) \
   Building_data[cur_id]->build_costs.push_back( Resource_amount( (t), (n) ) )
+#define _build_time(t) \
+  Building_data[cur_id]->build_time = (t)
 #define _upkeep(n) \
   Building_data[cur_id]->upkeep = (n)
 #define _maintenance(t, n) \
@@ -35,6 +37,7 @@ void init_building_data()
     _name("hovel");
     _cost(RES_GOLD, 100);
     _cost(RES_WOOD, 10);
+    _build_time(3);
     _upkeep(5);
     _housing(CIT_PEASANT, 100);
 
@@ -42,6 +45,7 @@ void init_building_data()
     _name("house");
     _cost(RES_GOLD, 400);
     _cost(RES_WOOD, 20);
+    _build_time(10);
     _upkeep(8);
     _housing(CIT_MERCHANT, 100);
 
@@ -49,6 +53,7 @@ void init_building_data()
     _name("manor");
     _cost(RES_GOLD, 800);
     _cost(RES_STONE, 20);
+    _build_time(30);
     _upkeep(12);
     _housing(CIT_BURGHER, 50);
 
@@ -56,6 +61,7 @@ void init_building_data()
     _name("keep");
     _cost(RES_GOLD, 1500);
     _cost(RES_STONE, 50);
+    _build_time(90);
     _upkeep(20);
     _housing(CIT_NOBLE, 20);
     _housing(CIT_BURGHER, 5);
@@ -65,6 +71,7 @@ void init_building_data()
   _build(BUILD_FARM);
     _name("farm");
     _cost(RES_GOLD, 100);
+    _build_time(1);
     _upkeep(3);
     _jobs(CIT_PEASANT, 10);
     _flat(RES_FARMING, 10);
@@ -72,6 +79,7 @@ void init_building_data()
   _build(BUILD_QUARRY);
     _name("quarry");
     _cost(RES_GOLD, 500);
+    _build_time(5);
     _upkeep(2);
     _jobs(CIT_PEASANT, 10);
     _flat(RES_STONE, 5);
@@ -80,6 +88,7 @@ void init_building_data()
     _name("mine");
     _cost(RES_GOLD, 500);
     _cost(RES_WOOD, 50);
+    _build_time(10);
     _upkeep(3);
     _jobs(CIT_PEASANT, 10);
     _flat(RES_MINING, 10);
@@ -87,12 +96,14 @@ void init_building_data()
   _build(BUILD_CAMP);
     _name("hunting camp");
     _cost(RES_GOLD, 50);
+    _build_time(1);
     _jobs(CIT_PEASANT, 10);
     _flat(RES_HUNTING, 10);
 
   _build(BUILD_SAWMILL);
     _name("sawmill");
     _cost(RES_GOLD, 100);
+    _build_time(5);
     _jobs(CIT_PEASANT, 10);
     _flat(RES_LOGGING, 10);
 
@@ -100,8 +111,8 @@ void init_building_data()
     _name("barracks");
     _cost(RES_GOLD, 500);
     _cost(RES_WOOD, 10);
+    _build_time(14);
 
-  
 }
 
 std::string Building_datum::get_short_description()
