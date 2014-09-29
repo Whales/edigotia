6b3e8353d25300bcc86a1f01667258a3945fefc6
Whales <fivedozenwhales@gmail.com>
2014-09-29 04:54:20 -0400

    Rudimentary struct for military units.

---
+++ military.cpp
@@ -0,0 +1,10 @@
+#include "military.h"
+
+Military_unit::Military_unit()
+{
+  count = 0;
+}
+
+Military_unit::~Military_unit()
+{
+}
---
+++ military.h
@@ -0,0 +1,12 @@
+#ifndef _MILITARY_H_
+#define _MILITARY_H_
+
+struct Military_unit
+{
+  Military_unit();
+  ~Military_unit();
+
+  int count;
+};
+
+#endif
