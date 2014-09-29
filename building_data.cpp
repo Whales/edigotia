1ba09613b4a480b19897aedf6d07a27e58821639
Whales <fivedozenwhales@gmail.com>
2014-09-29 04:47:46 -0400

    Can set buildings as plural.  Rename "hovel" -> "hovels" and "house" -> "houses."  Barracks support 50 soldiers.  Constructor/destructor for Building_datum.  Update Building_datum::get_short_description() to support vector of housing.

--- building_data.cpp
+++ building_data.cpp
@@ -5,22 +5,37 @@ Building_datum* Building_data[BUILD_MAX];
 
 #define _build(x) \
   cur_id = (x)
+
 #define _name(n) \
   Building_data[cur_id]->name = (n)
+
+#define _plural() \
+  Building_data[cur_id]->plural = true
+
 #define _cost(t, n) \
   Building_data[cur_id]->build_costs.push_back( Resource_amount( (t), (n) ) )
+
 #define _build_time(t) \
   Building_data[cur_id]->build_time = (t)
+
 #define _upkeep(n) \
   Building_data[cur_id]->upkeep = (n)
+
 #define _maintenance(t, n) \
   Building_data[cur_id]->maintenance_cost.push_back( Resource_amount( (t),(n) ))
+
 #define _housing(t, n) \
-  Building_data[cur_id]->housing = Citizen_amount((t), (n))
+  Building_data[cur_id]->housing.push_back( Citizen_amount((t), (n)) )
+
+#define _military(n) \
+  Building_data[cur_id]->military_support = (n)
+
 #define _jobs(t, n) \
   Building_data[cur_id]->jobs = Citizen_amount((t), (n))
+
 #define _flat(t, n) \
   Building_data[cur_id]->production.push_back( Resource_amount( (t), (n) ))
+
 #define _buildable(t) \
   Building_data[cur_id]->buildable.push_back( (t) )
 
@@ -34,7 +49,8 @@ void init_building_data()
   int cur_id;
 
   _build(BUILD_HOVEL);
-    _name("hovel");
+    _name("hovels");
+    _plural();
     _cost(RES_GOLD, 100);
     _cost(RES_WOOD, 10);
     _build_time(3);
@@ -42,7 +58,8 @@ void init_building_data()
     _housing(CIT_PEASANT, 100);
 
   _build(BUILD_HOUSE);
-    _name("house");
+    _name("houses");
+    _plural();
     _cost(RES_GOLD, 400);
     _cost(RES_WOOD, 20);
     _build_time(10);
@@ -63,7 +80,6 @@ void init_building_data()
     _cost(RES_STONE, 50);
     _build_time(90);
     _upkeep(20);
-    _housing(CIT_NOBLE, 20);
     _housing(CIT_BURGHER, 5);
     _housing(CIT_MERCHANT, 50);
     _housing(CIT_PEASANT, 100);
@@ -109,10 +125,25 @@ void init_building_data()
 
   _build(BUILD_BARRACKS);
     _name("barracks");
+    _plural();
     _cost(RES_GOLD, 500);
     _cost(RES_WOOD, 10);
     _build_time(14);
+    _military(50);
 
+}
+
+Building_datum::Building_datum()
+{
+  uid = -1;
+  plural = false;
+  upkeep = 0;
+  military_support = 0;
+  build_time = 0;
+}
+
+Building_datum::~Building_datum()
+{
 }
 
 std::string Building_datum::get_short_description()
@@ -146,9 +177,12 @@ std::string Building_datum::get_short_description()
     ret << "<c=/>" << std::endl;
   }
 
-  if (housing.amount > 0) {
-    ret << "<c=brown>Houses " << housing.amount << " " <<
-           citizen_type_name(housing.type) << "s.<c=/>" << std::endl;
+  if (!housing.empty()) {
+    ret << "<c=brown>Houses ";
+    for (int i = 0; i < housing.size(); i++) {
+      std::string cit_name = citizen_type_name(housing[i].type, true);
+      ret << housing[i].amount << " " << cit_name << "<c=/>" << std::endl;
+    }
   }
 
   if (jobs.amount > 0) {
