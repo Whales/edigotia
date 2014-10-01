3c1b402a4b346e1c974cecf679c8a6345f44e4a0
Whales <fivedozenwhales@gmail.com>
2014-10-01 01:05:21 -0400

    Put the testing check for resource placement on the world map into its own function.  Generate the City_map using its new function.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "game.h"
 #include "init.h"
 
+void check_world_resources(World_map* world);
+
 int main()
 {
 // seed the RNG.  TODO: wrap this
@@ -23,45 +25,30 @@ int main()
 // Generate a world...
   World_map world;
   world.generate();
-  int land = world.land_count();
-  int size = WORLD_MAP_SIZE * WORLD_MAP_SIZE;
-  float land_percent = float((100.0 * float(land)) / size);
-  debugmsg("Land tiles: %d/%d (%f percent)", land, size, land_percent);
-  for (int i = 0; i < CROP_MAX; i++) {
-    Crop crop = Crop(i);
-    int crop_count = world.crop_count(crop);
-    float crop_percent = float((100.0 * float(crop_count)) / size);
-    float crop_land_percent = float((100.0 * float(crop_count)) / land);
-    debugmsg("%s: %d/%d/%d (%f/%f percent, should be %d)",
-             Crop_data[crop]->name.c_str(), crop_count, land, size,
-             crop_land_percent, crop_percent, Crop_data[crop]->percentage);
-  }
-  for (int i = 0; i < MINERAL_MAX; i++) {
-    Mineral mineral = Mineral(i);
-    int mineral_count = world.mineral_count(mineral);
-    float mineral_percent = float((100.0 * float(mineral_count)) / size);
-    float mineral_land_percent = float((100.0 * float(mineral_count)) / land);
-    debugmsg("%s: %d/%d/%d (%f/%f percent, should be %d)",
-             Mineral_data[mineral]->name.c_str(), mineral_count, land, size,
-             mineral_land_percent, mineral_percent,
-             Mineral_data[mineral]->percentage);
-  }
+// Tool for debugging / testing the amount of resources in the world.
+  check_world_resources(&world);
 // ... for now, we always start by picking a location for a new city.
   Point p = world.draw();
 
-  if (p.x == -1) {
+  if (p.x == -1) {  // We hit ESC
     return 0;
   }
 
 // Put our city there.
   City city;
-  city.map.generate( world.tiles[p.x][p.y], world.coast_from(p.x, p.y));
+  std::vector<Crop>    crops    = world.crops_at   (p);
+  std::vector<Mineral> minerals = world.minerals_at(p);
+  city.map.generate( world.tiles[p.x][p.y], crops, minerals,
+                     world.coast_from(p.x, p.y)             );
   bool placed = false;
   while (!placed) {
     placed = city.place_keep();
     if (!placed) {
       p = world.draw();
-      city.map.generate( world.tiles[p.x][p.y], world.coast_from(p.x, p.y));
+      crops    = world.crops_at   (p);
+      minerals = world.minerals_at(p);
+      city.map.generate( world.tiles[p.x][p.y], crops, minerals,
+                         world.coast_from(p.x, p.y)             );
     }
   }
 
@@ -78,3 +65,29 @@ int main()
   return 0;
 }
 
+void check_world_resources(World_map* world)
+{
+  int land = world->land_count();
+  int size = WORLD_MAP_SIZE * WORLD_MAP_SIZE;
+  float land_percent = float((100.0 * float(land)) / size);
+  debugmsg("Land tiles: %d/%d (%f percent)", land, size, land_percent);
+  for (int i = 0; i < CROP_MAX; i++) {
+    Crop crop = Crop(i);
+    int crop_count = world->crop_count(crop);
+    float crop_percent = float((100.0 * float(crop_count)) / size);
+    float crop_land_percent = float((100.0 * float(crop_count)) / land);
+    debugmsg("%s: %d/%d/%d (%f/%f percent, should be %d)",
+             Crop_data[crop]->name.c_str(), crop_count, land, size,
+             crop_land_percent, crop_percent, Crop_data[crop]->percentage);
+  }
+  for (int i = 0; i < MINERAL_MAX; i++) {
+    Mineral mineral = Mineral(i);
+    int mineral_count = world->mineral_count(mineral);
+    float mineral_percent = float((100.0 * float(mineral_count)) / size);
+    float mineral_land_percent = float((100.0 * float(mineral_count)) / land);
+    debugmsg("%s: %d/%d/%d (%f/%f percent, should be %d)",
+             Mineral_data[mineral]->name.c_str(), mineral_count, land, size,
+             mineral_land_percent, mineral_percent,
+             Mineral_data[mineral]->percentage);
+  }
+}
