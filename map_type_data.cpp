7d41f302523d0d0ef499ceecb8249141613eacc1
Whales <fivedozenwhales@gmail.com>
2014-10-03 15:33:54 -0400

    Macros _water() and _river() turn on water/river status for Map_type_datums.  Include a datum for MAP_NULL.

--- map_type_data.cpp
+++ map_type_data.cpp
@@ -6,6 +6,10 @@ Map_type_datum* Map_type_data[MAP_MAX];
   cur_id = n
 #define _name(n) \
   Map_type_data[cur_id]->name = n
+#define _water() \
+  Map_type_data[cur_id]->is_water = true
+#define _river() \
+  Map_type_data[cur_id]->is_river = true
 #define _symbol(c, f) \
   Map_type_data[cur_id]->symbol = glyph(c, f, c_black)
 
@@ -15,6 +19,10 @@ void init_map_type_data()
   for (int i = 0; i < MAP_MAX; i++) {
     Map_type_data[i] = new Map_type_datum;
   }
+
+  _ter(MAP_NULL);
+    _name("Out of bounds");
+    _symbol('x', c_red);
 
   _ter(MAP_TUNDRA);
     _name("tundra");
@@ -38,6 +46,7 @@ void init_map_type_data()
 
   _ter(MAP_SWAMP);
     _name("swamp");
+    _water();
     _symbol('s', c_cyan);
 
   _ter(MAP_FOOTHILLS);
@@ -58,14 +67,20 @@ void init_map_type_data()
 
   _ter(MAP_BASIN);
     _name("river basin");
+    _river();
+    _water();
     _symbol('S', c_blue);
 
   _ter(MAP_CANYON);
     _name("canyon");
+    _river();
+    _water();
     _symbol('S', c_ltblue);
 
   _ter(MAP_GLACIER);
     _name("glacier");
+    _river();
+    _water();
     _symbol('S', c_ltcyan);
 
   _ter(MAP_COASTAL);
@@ -74,6 +89,7 @@ void init_map_type_data()
 
   _ter(MAP_OCEAN);
     _name("ocean");
+    _water();
     _symbol('=', c_blue);
 
   _ter(MAP_ICECAP);
