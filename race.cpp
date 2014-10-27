4db7d9afddb8720bf733e04424c456d2da4a22ee
Whales <fivedozenwhales@gmail.com>
2014-10-27 03:28:15 -0400

    Improve Race_datum::Race_datum().

--- race.cpp
+++ race.cpp
@@ -10,11 +10,17 @@ Race_datum::Race_datum()
   for (int i = 0; i < CIT_MAX; i++) {
     starting_population[i]  = 0;
     birth_rate[i]           = 100;
-    citizen_ratio[i]        = 10;
-    morale_requirement[i]   = 50;
+    citizen_ratio[i]        = 0;
+    morale_requirement[i]   = 0;
     low_tax_rate[i]         = 10;
     high_tax_rate[i]        = 70;
   }
+
+// Some special cases...
+  citizen_ratio[CIT_MERCHANT] = 10;
+  citizen_ratio[CIT_BURGHER]  = 10;
+  morale_requirement[CIT_MERCHANT] = 50;
+  morale_requirement[CIT_BURGHER]  = 50;
 
   food_consumption = 100;
 
