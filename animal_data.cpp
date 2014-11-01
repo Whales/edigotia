00cd1db42c3063cbf1895ce0930cbb6049a8c7fd
Whales <fivedozenwhales@gmail.com>
2014-11-01 01:31:22 -0400

    Add range data (preferred temperature, altitude, and rainfall) for animals.

--- animal_data.cpp
+++ animal_data.cpp
@@ -8,6 +8,17 @@ Animal_datum* Animal_data[ANIMAL_MAX];
 #define _name(n) \
   Animal_data[cur_id]->name = (n)
 
+#define _temp_range(a, b) \
+  Animal_data[cur_id]->min_temp = (a); Animal_data[cur_id]->max_temp = (b)
+
+#define _altitude_range(a, b) \
+  Animal_data[cur_id]->min_altitude = (a); \
+  Animal_data[cur_id]->max_altitude = (b) 
+
+#define _rainfall_range(a, b) \
+  Animal_data[cur_id]->min_rainfall = (a); \
+  Animal_data[cur_id]->max_rainfall = (b)
+
 #define _danger(n) \
   Animal_data[cur_id]->danger = (n)
 
@@ -51,6 +62,9 @@ void init_animal_data()
 
   _animal(ANIMAL_CHICKEN);
     _name("chicken");
+    _temp_range     ( 25,  80);
+    _altitude_range (  0,  85);
+    _rainfall_range ( 15,  50);
     _reproduction(45);
     _hardiness(1800);
     _food_killed(2);
@@ -60,6 +74,9 @@ void init_animal_data()
 
   _animal(ANIMAL_PIG);
     _name("pig");
+    _temp_range     ( 35,  75);
+    _altitude_range (  0,  85);
+    _rainfall_range ( 20,  70);
     _reproduction(32);
     _hardiness(2800);
     _food_killed(60);
@@ -68,6 +85,9 @@ void init_animal_data()
 
   _animal(ANIMAL_SHEEP);
     _name("sheep");
+    _temp_range     ( 10,  60);
+    _altitude_range (  0,  95);
+    _rainfall_range ( 15,  60);
     _reproduction(30);
     _hardiness(2500);
     _food_killed(50);
@@ -78,6 +98,9 @@ void init_animal_data()
 
   _animal(ANIMAL_COW);
     _name("cow");
+    _temp_range     ( 20,  85);
+    _altitude_range (  0,  85);
+    _rainfall_range ( 15,  60);
     _reproduction(30);
     _hardiness(3500);
     _food_killed(250);
@@ -88,6 +111,9 @@ void init_animal_data()
 
   _animal(ANIMAL_HORSE);
     _name("horse");
+    _temp_range     ( 20,  80);
+    _altitude_range (  0,  80);
+    _rainfall_range ( 15,  60);
     _danger(1);
     _reproduction(20);
     _hardiness(4000);
@@ -101,6 +127,9 @@ void init_animal_data()
 
   _animal(ANIMAL_HARE);
     _name("hare");
+    _temp_range     ( 10,  60);
+    _altitude_range (  0,  90);
+    _rainfall_range ( 10,  75);
     _reproduction(50);
     _hardiness(800);
     _food_killed(2);
@@ -110,6 +139,9 @@ void init_animal_data()
 
   _animal(ANIMAL_FOX);
     _name("fox");
+    _temp_range     (  5,  90);
+    _altitude_range (  0,  85);
+    _rainfall_range (  5,  60);
     _reproduction(25);
     _hardiness(2000);
     _food_killed(15);
@@ -120,6 +152,9 @@ void init_animal_data()
 
   _animal(ANIMAL_BOAR);
     _name("boar");
+    _temp_range     ( 35,  90);
+    _altitude_range (  0,  60);
+    _rainfall_range (  0,  70);
     _danger(5);
     _reproduction(28);
     _hardiness(3400);
@@ -129,6 +164,9 @@ void init_animal_data()
 
   _animal(ANIMAL_DEER);
     _name("deer");
+    _temp_range     ( 12,  85);
+    _altitude_range (  0,  90);
+    _rainfall_range ( 10,  60);
     _reproduction(20);
     _hardiness(3000);
     _food_killed(80);
@@ -141,6 +179,9 @@ void init_animal_data()
 
   _animal(ANIMAL_BEAR);
     _name("bear");
+    _temp_range     (  0,  55);
+    _altitude_range (  5,  80);
+    _rainfall_range ( 15,  65);
     _danger(10);
     _reproduction(18);
     _hardiness(3400);
@@ -151,6 +192,9 @@ void init_animal_data()
 
   _animal(ANIMAL_WOLF);
     _name("wolf");
+    _temp_range     (  5,  60);
+    _altitude_range (  0,  75);
+    _rainfall_range ( 15,  60);
     _danger(8);
     _reproduction(25);
     _hardiness(3000);
@@ -162,6 +206,9 @@ void init_animal_data()
 
   _animal(ANIMAL_LION);
     _name("lion");
+    _temp_range     ( 70, 110);
+    _altitude_range (  0,  50);
+    _rainfall_range (  0,  25);
     _danger(12);
     _reproduction(15);
     _hardiness(3500);
@@ -174,6 +221,9 @@ void init_animal_data()
 // Legit question: could we drink elephant milk?
   _animal(ANIMAL_ELEPHANT);
     _name("elephant");
+    _temp_range     ( 65, 100);
+    _altitude_range (  0,  60);
+    _rainfall_range (  0,  30);
     _danger(16);
     _reproduction(8);
     _hardiness(7000);
@@ -187,6 +237,9 @@ void init_animal_data()
 
   _animal(ANIMAL_JELLY);
     _name("jelly");
+    _temp_range     ( 32,  75);
+    _altitude_range (  0,  80);
+    _rainfall_range ( 40,  90);
     _danger(2);
     _reproduction(50);
     _hardiness(600);
@@ -198,6 +251,9 @@ void init_animal_data()
 
   _animal(ANIMAL_DIRE_WOLF);
     _name("dire wolf");
+    _temp_range     (  2,  60);
+    _altitude_range (  0,  90);
+    _rainfall_range ( 15,  70);
     _danger(16);
     _reproduction(18);
     _hardiness(5000);
@@ -209,6 +265,9 @@ void init_animal_data()
 
   _animal(ANIMAL_MANTICORE);
     _name("manticore");
+    _temp_range     ( 50, 110);
+    _altitude_range ( 60, 100);
+    _rainfall_range (  0,  40);
     _danger(18);
     _reproduction(15);
     _hardiness(7500);
@@ -219,6 +278,9 @@ void init_animal_data()
 
   _animal(ANIMAL_UNICORN);
     _name("unicorn");
+    _temp_range     ( 30,  65);
+    _altitude_range (  0,  95);
+    _rainfall_range ( 40,  60);
     _danger(15);
     _reproduction(5);
     _hardiness(8000);
@@ -229,6 +291,9 @@ void init_animal_data()
 
   _animal(ANIMAL_BASILISK);
     _name("basilisk");
+    _temp_range     ( 60, 110);
+    _altitude_range (  0, 100);
+    _rainfall_range (  0,  40);
     _danger(20);
     _reproduction(8);
     _hardiness(7000);
@@ -238,6 +303,9 @@ void init_animal_data()
 
   _animal(ANIMAL_WYVERN);
     _name("wyvern");
+    _temp_range     ( 30,  90);
+    _altitude_range ( 70, 100);
+    _rainfall_range ( 10,  70);
     _danger(22);
     _reproduction(15);
     _hardiness(9000);
