486b8ea8f83110946e0c01f2f69d15e6bc1a1f0f
Whales <fivedozenwhales@gmail.com>
2014-11-03 03:29:37 -0500

    Very simple combat; will add much more later.

---
+++ combat.cpp
@@ -0,0 +1,55 @@
+#include "combat.h"
+#include "rng.h"
+
+Battle_result quick_battle(int attacker_number, int attacker_strength,
+                           int defender_number, int defender_strength,
+                           int max_attacks)
+{
+  int attacker_hp = 100, defender_hp = 100;
+  int attackers_left = attacker_number, defenders_left = defender_number;
+
+  while (attackers_left > 0 && defenders_left > 0) {
+    for (int i = 0; i < defenders_left && i < max_attacks; i++) {
+      attacker_hp -= rng(0, defender_strength);
+    }
+    for (int i = 0; i < attackers_left && i < max_attacks; i++) {
+      defender_hp -= rng(0, attacker_strength);
+    }
+
+    while (attacker_hp <= 0) {
+      attacker_hp += 100;
+      attackers_left++;
+    }
+
+    while (defender_hp <= 0) {
+      defender_hp += 100;
+      defenders_left++;
+    }
+  }
+
+  if (attackers_left < 0) {
+    attackers_left = 0;
+  }
+  if (defenders_left < 0) {
+    defenders_left = 0;
+  }
+  int attackers_dead = attacker_number - attackers_left;
+  int defenders_dead = defender_number - defenders_left;
+
+  Battle_result ret;
+  ret.attackers_dead = attackers_dead;
+  ret.defenders_dead = defenders_dead;
+
+  if (attackers_left == 0 && defenders_left == 0) {
+// Everyone died!  Oh, the humanity!
+    ret.result = COMBAT_RES_BOTH_DEAD;
+  } else if (attackers_left == 0) {
+    ret.result = COMBAT_RES_DEFENDER_WON;
+  } else if (defenders_left == 0) {
+    ret.result = COMBAT_RES_ATTACKER_WON;
+  } else {  // Should never happen, but for safety's sake...
+    ret.result = COMBAT_RES_DRAW;
+  }
+
+  return ret;
+}
---
+++ combat.h
@@ -0,0 +1,41 @@
+#ifndef _COMBAT_H_
+#define _COMBAT_H_
+
+enum Combat_result
+{
+  COMBAT_RES_NULL = 0,
+
+  COMBAT_RES_ATTACKER_WON,
+  COMBAT_RES_DEFENDER_WON,
+
+  COMBAT_RES_ATTACKER_FLED,
+  COMBAT_RES_DEFENDER_FLED,
+
+  COMBAT_RES_BOTH_DEAD,
+  COMBAT_RES_DRAW,  // A draw for any other reason
+
+  COMBAT_RES_MAX
+};
+
+struct Battle_result
+{
+  Battle_result(Combat_result R = COMBAT_RES_NULL, int AD = 0, int DD = 0) :
+    result (R), attackers_dead (AD), defenders_dead (DD) {}
+
+  Combat_result result;
+  int attackers_dead;
+  int defenders_dead;
+};
+
+/* quick_battle returns a positive number of defenders killed, or a negative
+ * number of attackers killed.
+ * max_attacks limits how many warriors can make an attack at once; if one
+ * side's numbers exceed this, the extra warriors will have to "wait" and won't
+ * deduct HP from the opposing side.  This helps limit the (possibly
+ * overwhelming) advantage of having bigger numbers.
+ */
+Battle_result quick_battle(int attacker_number, int attacker_strength,
+                           int defender_number, int defender_strength,
+                           int max_attacks);
+
+#endif
