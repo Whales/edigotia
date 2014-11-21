ba0e311c143089bde9816792c253b8470c3d449d
Whales <fivedozenwhales@gmail.com>
2014-11-20 23:45:21 -0500

    New function AI_city::randomize_properties() randomly sets our population and picks an appropriate City_role.

--- ai_city.cpp
+++ ai_city.cpp
@@ -1,5 +1,7 @@
 #include "ai_city.h"
 #include "window.h" // For debugmsg()
+#include "world.h"  // Needed for randomize_properties()
+#include "rng.h"
 
 AI_city::AI_city()
 {
@@ -8,14 +10,59 @@ AI_city::AI_city()
   for (int i = 0; i < CIT_MAX; i++) {
     population[i].type = Citizen_type(i);
   }
-
-  population[CIT_PEASANT].add_citizens(150);
 }
 
 AI_city::~AI_city()
 {
 }
 
-// TODO: This is a duplicate of City::get_total_population().  They should
-//       probably both be children of the same parent class.
-// type defaults to CIT_NULL
+void AI_city::randomize_properties(World_map* world)
+{
+  if (!world) {
+    debugmsg("AI_city::randomize_properties(NULL) called!");
+    return;
+  }
+
+  Race_datum* race_dat = Race_data[race];
+
+// Set up our populations
+  for (int i = 0; i < CIT_MAX; i++) {
+    population[i].reset();
+  }
+
+  int pop = rng(race_dat->city_size_min[type], race_dat->city_size_max[type]);
+// Figure out how to arrange pop, using the ratios from our race
+/* Quick explanation of the equations below:
+ * Let X = peasant pop, Y = merchant pop, Z = burgher pop, N = total pop
+ * Let a = peasant:merchant ratio, b = merchant:burgher ratio.
+ * Thus X = aY, Y = bZ, and N = X + Y + Z
+ * Z = (N - Z) / (a * b)                    ===>   Z = N / (a * b + 1)
+ * Y = X / a   ===>   Y = (N - Y - Z) / a   ===>   Y = (N - Z) / (a + 1)
+ * X = N - Y - Z
+ */
+
+  int ratio_a = race_dat->citizen_ratio[CIT_MERCHANT],
+      ratio_b = race_dat->citizen_ratio[CIT_BURGHER];
+  if (ratio_a == 0 || ratio_b == 0) {
+    debugmsg("Bad citizen_ratio in %s data!", race_dat->name.c_str());
+    return;
+  }
+  int burghers  = pop / (1 + ratio_a * ratio_b);
+  int merchants = (pop - burghers) / (1 + ratio_a);
+  int peasants = pop - merchants - burghers;
+
+  population[CIT_PEASANT].add_citizens(peasants);
+  population[CIT_MERCHANT].add_citizens(merchants);
+  population[CIT_BURGHER].add_citizens(burghers);
+
+// Now, pick a City_role from our terrain.
+  Map_type mtype = world->get_map_type(location);
+  Map_type_datum* map_dat = Map_type_data[mtype];
+
+  if (map_dat->city_roles.empty()) {
+    role = CITY_ROLE_NULL;  // daaaaarn
+  } else {
+    int index = rng(0, map_dat->city_roles.size() - 1);
+    role = map_dat->city_roles[index];
+  }
+}
--- ai_city.h
+++ ai_city.h
@@ -4,6 +4,9 @@
 #include "citizen.h"
 #include "city.h"
 #include "glyph.h"  // For AI_city::get_glyph()
+#include "ai.h"     // For City_role
+
+class World_map;
 
 class AI_city : public City
 {
@@ -12,6 +15,13 @@ public:
   virtual ~AI_city();
 
   virtual bool is_ai_city() { return true; }
+
+// Randomize population, role, and more
+  void randomize_properties(World_map* world);
+
+private:
+  City_role role;
+
 };
 
 #endif
