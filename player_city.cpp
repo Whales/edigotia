59563f60387b561a39bb3c1ce24a5cdb128ca8cb
Whales <fivedozenwhales@gmail.com>
2014-11-20 23:17:13 -0500

    Move City_type and related functions to its own files.

--- player_city.cpp
+++ player_city.cpp
@@ -2225,27 +2225,3 @@ nc_color message_type_color(Message_type type)
   return c_magenta;
 }
 
-City_type lookup_city_type(std::string name)
-{
-  name = no_caps( trim( name ) );
-  for (int i = 0; i < CITY_TYPE_MAX; i++) {
-    City_type ret = City_type(i);
-    if (name == no_caps( trim( city_type_name(ret) ) ) ) {
-      return ret;
-    }
-  }
-  return CITY_TYPE_NULL;
-}
-
-std::string city_type_name(City_type type)
-{
-  switch (type) {
-    case CITY_TYPE_NULL:    return "NULL";
-    case CITY_TYPE_CITY:    return "city";
-    case CITY_TYPE_DUCHY:   return "duchy seat";
-    case CITY_TYPE_CAPITAL: return "capital";
-    case CITY_TYPE_MAX:     return "BUG - CITY_TYPE_MAX";
-    default:                return "BUG - Unnamed City_type";
-  }
-  return "BUG - Escaped city_type_name() switch!";
-}
