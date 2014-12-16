30fda08f03cf480ee3ea019ce3c1a62082a31837
Whales <fivedozenwhales@gmail.com>
2014-12-16 18:00:03 -0500

    Fix a == where it should be =.  Include skill requirements and food percentages.  Default constructor for City_role_datum.  New city roles, hunting and livestock.

--- ai_data.cpp
+++ ai_data.cpp
@@ -4,7 +4,14 @@
   cur_id = (i)
 
 #define _name(s) \
-  City_role_data[cur_id]->name == (s)
+  City_role_data[cur_id]->name = (s)
+
+#define _skill(s, n) \
+  City_role_data[cur_id]->skill = (s); \
+  City_role_data[cur_id]->skill_req = (n)
+
+#define _food_percentage(n) \
+  City_role_data[cur_id]->food_percentage = (n)
 
 #define _resource_export(r) \
   City_role_data[cur_id]->resource_exports.push_back( (r) )
@@ -20,6 +27,14 @@
 
 City_role_datum* City_role_data[CITY_ROLE_MAX];
 
+City_role_datum::City_role_datum()
+{
+  name = "Unitialized Role";
+  food_percentage = 90; // Most roles require some imports
+  skill = SKILL_NULL;
+  skill_req = 0;
+}
+
 void init_city_roles()
 {
   for (int i = 0; i < CITY_ROLE_MAX; i++) {
@@ -27,12 +42,28 @@ void init_city_roles()
   }
   int cur_id = 0;
 
+  _role(CITY_ROLE_NULL);
+    _name("none");
+    _food_percentage(100);
+
   _role(CITY_ROLE_FARMING);
     _name("farming");
-    _resource_export(RES_FOOD);
+    _skill(SKILL_FARMING, 3);
+    _food_percentage(125);
+
+  _role(CITY_ROLE_HUNTING);
+    _name("hunting");
+    _skill(SKILL_HUNTING, 2); // We're forgiving!
+    _food_percentage(120);
+
+  _role(CITY_ROLE_LIVESTOCK);
+    _name("livestock");
+    _skill(SKILL_LIVESTOCK, 3);
+    _food_percentage(120);
 
   _role(CITY_ROLE_MINING);
     _name("mining");
+    _skill(SKILL_MINING, 3);
     _resource_export(RES_STONE);
     _mineral_export(MINERAL_TIN);
     _mineral_export(MINERAL_COPPER);
@@ -41,5 +72,6 @@ void init_city_roles()
 
   _role(CITY_ROLE_LOGGING);
     _name("logging");
+    _skill(SKILL_FORESTRY, 2);
     _resource_export(RES_WOOD);
 }
