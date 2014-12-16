6e1bd22fd02636a53e298870010e8088afeb6015
Whales <fivedozenwhales@gmail.com>
2014-12-16 17:59:02 -0500

    New city roles, hunting and livestock.  City_role_datum now (optionally) has a required skill level (e.g. only races with farming skill >= 3 will build farming cities), and a target food percentage; the percentage of our required food that we try to produce (defaults to 90, farming/hunting/livestock cities use > 100).

--- ai.h
+++ ai.h
@@ -2,8 +2,8 @@
 #define _AI_H_
 
 #include "resource.h"
+#include "race.h"
 #include <vector>
-
 
 // City_role is an attempt to abstract what makes an AI city
 // "roleial."  It primarily affects their imports and exports, but may also
@@ -14,6 +14,8 @@ enum City_role
   CITY_ROLE_NULL = 0,     // No role
 
   CITY_ROLE_FARMING,      // Food production - exports lots of food
+  CITY_ROLE_HUNTING,      // Exports food, leather and fur
+  CITY_ROLE_LIVESTOCK,    // Exports food, leather, fur, and animals
   CITY_ROLE_MINING,       // Mining - exports ores
   CITY_ROLE_LOGGING,      // Logging - exports wood
 
@@ -22,7 +24,21 @@ enum City_role
 
 struct City_role_datum
 {
+  City_role_datum();
+
   std::string name;
+
+// skill defaults to SKILL_NULL; if it's something else, then we won't use this
+// role unless our race's skill is at least skill_req
+  Race_skill skill;
+  int skill_req;
+
+/* food_percentage is the amount of our required food that we produce.
+ * If it's less than 100, we need to import some food.  If it's more than 100,
+ * we'reaiming to exportsome food.  If it's exactly 100, we will try to produce
+ * exactly what we need, but may wind up some some import or exports anyway.
+ */
+  int food_percentage;
 
   std::vector<Resource> resource_exports;
   std::vector<Resource> resource_imports;
