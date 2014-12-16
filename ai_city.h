1b55a8358eb0e101b3be8f81cc7da905e4c64f76
Whales <fivedozenwhales@gmail.com>
2014-12-16 18:09:48 -0500

    New functions for AI_city: add_farms(), add_hunting_camps(), add_pastures(), add_mines() and add_sawmills() all "add" areas; in reality, they just remove a map tile from availability and add the appropriate resources.  AI_city::add_resource_production() and AI_city::add_mineral_production() make adding new production easier.  AI_city also tracks free peasants (which are required to add new areas via the above functions) and its mineral production.

--- ai_city.h
+++ ai_city.h
@@ -25,6 +25,29 @@ public:
   int get_net_food(); // production - consumption, obvs
 
 private:
+// add_farms(), add_hunting_camps(), and add_pastures() have two forms; one with
+// an int& and one without.  Use the one WITH the int& if you want to stop
+// placing the areas once we reach that amount of food (50,000 times the amount
+// we actually want, to avoid rounding errors).  It will modify the int& passed
+// to reflect the amount of food gained.
+// Use the form WITHOUT the int& if you want to keep placing areas until we're
+// no longer able to (thus producing as much food as possible).
+  void add_farms        (std::vector<Map_tile*>& tiles, int& food_req);
+  void add_hunting_camps(std::vector<Map_tile*>& tiles, int& food_req);
+  void add_pastures     (std::vector<Map_tile*>& tiles, int& food_req);
+  void add_farms        (std::vector<Map_tile*>& tiles);
+  void add_hunting_camps(std::vector<Map_tile*>& tiles);
+  void add_pastures     (std::vector<Map_tile*>& tiles);
+// These ones don't involve food, so they just need our list of tiles.
+  void add_mines        (std::vector<Map_tile*>& tiles);
+  void add_sawmills     (std::vector<Map_tile*>& tiles);
+
+  void add_resource_production(Resource_amount res_amt);
+  void add_resource_production(Resource res, int amount);
+
+  void add_mineral_production(Mineral_amount min_amt);
+  void add_mineral_production(Mineral min, int amount);
+
   City_role role;
 
 /* City control radius, like the player uses, except we only use it to figure
@@ -32,7 +55,9 @@ private:
  * based on our population.
  */
   int radius;
+  int free_peasants;
   std::map<Resource,int> resource_production;
+  std::map<Mineral,int>  mineral_production;
 
 };
 
