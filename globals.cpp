b6912d76aabbd61df5cdac2bd552bfc3adbc02da
Whales <fivedozenwhales@gmail.com>
2015-01-27 18:54:14 -0500

    Use a global SAVE_DIR for location of saved data.

--- globals.cpp
+++ globals.cpp
@@ -1,3 +1,17 @@
 #include "globals.h"
+#include "files.h"
+#include "window.h" // For debugmsg()
 
 Game* GAME;
+
+std::string SAVE_DIR;
+
+bool set_directories()
+{
+  SAVE_DIR = "save/";
+  if (!directory_exists(SAVE_DIR) && !create_directory(SAVE_DIR)) {
+    debugmsg("Unable to create directory '%s'.", SAVE_DIR.c_str());
+    return false;
+  }
+  return true;
+}
--- globals.h
+++ globals.h
@@ -1,8 +1,13 @@
 #ifndef _GLOBALS_H_
 #define _GLOBALS_H_
 
+#include <string>
+
 class Game;
 
 extern Game* GAME;
+extern std::string SAVE_DIR;
+
+bool set_directories();
 
 #endif
