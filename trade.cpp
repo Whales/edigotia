ced3b828a17b1e1632f5e804599a5dcd7d27f840
Whales <fivedozenwhales@gmail.com>
2015-02-25 23:46:02 -0500

    Include a Point target in Trade_route.

--- trade.cpp
+++ trade.cpp
@@ -6,14 +6,17 @@ Trade_route::Trade_route()
   distance = -1;
 }
 
-Trade_route::Trade_route(int _distance)
+// _distance defaults to -1
+Trade_route::Trade_route(Point _target, int _distance)
 {
+  target = _target;
 // TODO: Warn if _distance <= 0?
   distance = _distance;
 }
 
 Trade_route::Trade_route(const Trade_route& other)
 {
+  target = other.target;
   distance = other.distance;
 }
 
@@ -24,14 +27,16 @@ Trade_route::~Trade_route()
 std::string Trade_route::save_data()
 {
   std::stringstream ret;
-  ret << distance;
+  ret << target.x << " ";
+  ret << target.y << " ";
+  ret << distance << " ";
 
   return ret.str();
 }
 
 bool Trade_route::load_data(std::istream& data)
 {
-  data >> distance;
+  data >> target.x >> target.y >> distance;
 
   return true;
 }
--- trade.h
+++ trade.h
@@ -1,6 +1,7 @@
 #ifndef _TRADE_H_
 #define _TRADE_H_
 
+#include "geometry.h" // for Point
 #include <string>
 #include <istream>
 
@@ -11,13 +12,14 @@ class City;
 struct Trade_route
 {
   Trade_route();
-  Trade_route(int _distance);
+  Trade_route(Point _target, int _distance = -1);
   Trade_route(const Trade_route& other);
   ~Trade_route();
 
   std::string save_data();
   bool load_data(std::istream& data);
 
+  Point target;
 /* distance is not physical - it's the "trade distance," measured by the ease of
  * getting a caravan there.  Basically the same as the total road cost on an A*
  * route to our destination.
